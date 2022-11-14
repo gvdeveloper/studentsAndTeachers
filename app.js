adb9e1bf47b5aa867f1064b84ba0bc953beb691e
gvdeveloper <gonzalovono@gmail.com>
2022-11-14 16:03:28 +0100

    1-Base

---
+++ app.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const cardStudents = document.getElementById('cardStudents');
+const cardTeachers = document.getElementById('cardTeachers');
+const templateStudent = document.getElementById('templateStudent').content;
+const templateTeachers = document.getElementById('templateTeachers').content;
